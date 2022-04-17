2b51a1b621a65787f92c514c1ce6ff87cae7fb0a
Felipegomeze1 <100251104+Felipegomeze1@users.noreply.github.com>
2022-04-17 16:29:50 -0500

    Update ProyectoDatos.py

--- codigo/ProyectoDatos.py
+++ codigo/ProyectoDatos.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 22 16:08:49 2022
-
-@author: felipegomez
-"""
+import pandas as pd
+import numpy as np
+import networkx as nx
 
 archivo = open('calles_de_medellin_con_acoso.csv', "r")
 lineas = archivo.readlines()
 elementos = []
 for linea in lineas:
     elementos.append(linea.split(";"))
-    
 
 
-
+print("Ingrese su lugar de inicio.")
+inicio=input()
+print("Ingrese el lugar al que quiere llegar.")
+objetivo=input()
+df = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=";")
+mapa = nx.from_pandas_edgelist(df,source='origin',target='destination',edge_attr='length')
+camino = nx.dijkstra_path(mapa, source=inicio, target=objetivo, weight=True)
+print("El camino más corto para llegar a su destino es:")
+print(camino)
